6083d947725749042b2894e28bf6761990cddef7
Adeola Oyetunde <adeolaoyetundeaa@gmail.com>
2025-05-06 19:18:39 +0100

    We created servers, modules and worked on integrat ion with database

---
+++ server/db.js
@@ -0,0 +1,27 @@
+const { Pool } = require("pg");
+require("dotenv").config();
+
+const mycon = new Pool({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+  max: 20, // Max connections
+  idleTimeoutMillis: 30000, // Close idle connections after 30s
+  connectionTimeoutMillis: 2000, // Fail if connection takes >2s
+  options: "-c search_path=data",
+});
+
+async function queryWithPool() {
+  try {
+    const res = await mycon.query("SELECT * FROM oau_poi WHERE id = $1", [1]);
+    console.log("blocks-1:", res.rows[0]);
+  } catch (err) {
+    console.error("Error:", err);
+  }
+}
+
+queryWithPool();
+
+module.exports = mycon;
